ed8549c1c1ff2bc50b1f458ddcbde6de441f24f5
Mehmet <mehmetyazici.3407@gmail.com>
2023-11-05 02:41:16 +0300

    adds test main operation bean

--- src/main/java/com/example/application/SpringKafkaApplication.java
+++ src/main/java/com/example/application/SpringKafkaApplication.java
@@ -1,32 +1,31 @@
 package com.example.application;
 
+import com.example.application.component.KafkaConsumer;
+import com.example.application.component.KafkaProducer;
+import com.example.application.component.model.MyEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 @Slf4j
+@RequiredArgsConstructor
 public class SpringKafkaApplication {
+
+    private final KafkaProducer kafkaProducer;
+    private final KafkaConsumer kafkaConsumer;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringKafkaApplication.class, args);
     }
 
     @Bean
-    public NewTopic topic() {
-        return TopicBuilder.name("topic1")
-                .partitions(10)
-                .replicas(1)
-                .build();
-    }
-
-    @KafkaListener(id = "myId", topics = "topic1")
-    public void listen(String in) {
-        log.info("Message: " + in);
+    public void test() {
+        MyEvent myEvent = new MyEvent("Hello!");
+        kafkaProducer.sendFlightEvent(myEvent);
+        kafkaConsumer.flightEventConsumer(myEvent);
     }
 
 }
