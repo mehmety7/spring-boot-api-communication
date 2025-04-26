d246bd65b3cf76c9f11281bd597b05c7ac4ba9b7
Mehmet <mehmetyazici.3407@gmail.com>
2025-04-27 02:54:07 +0300

    refactors naming and properties

--- src/test/java/com/example/application/SpringAPICommunicationApplicationTests.java
+++ src/test/java/com/example/application/SpringAPICommunicationApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.application;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringKafkaApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
+class SpringAPICommunicationApplicationTests {
 
 }
